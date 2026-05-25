1feadf34466f299bbcd9965015d183daeb649d18
vinit-jain713 <jainvinit713@gmail.com>
2026-05-25 23:05:22 +0530

    Responsiv Navbar and Hero Section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,11 +4,11 @@ import Button from '../Button/Button'
 const Hero = () => {
     return (
         <section>
-            <div className='max-w-[1200px] min-h-screen mx-auto px-10 flex gap-20 items-center pt-25'>
+            <div className='max-w-[1200px] min-h-screen mx-auto px-10 flex md:flex-row flex-col md:gap-20 items-center md:pt-20 pt-30'>
                 <div className='flex-1'>
                     <span className='bg-orange-100 text-orange-500 text-lg px-5 py-2 rounded-full'>Export Best Quality...</span>
-                    <h1 className='text-6xl/18 font-bold mt-3'>
-                        Tasty Organic <br/><span className='text-orange-500'>Fruits</span> & <span className='text-orange-500'>Veggies</span><br/> In Your City
+                    <h1 className='md:text-6xl/18 text-4xl font-bold mt-3'>
+                        Healthy Organic <br/><span className='text-orange-500'>Fruits</span> & <span className='text-orange-500'>Veggies</span><br/> In Your City
                     </h1>
                     <p className='text-zinc-600 text-lg mb-5'>
                         Fresh groceries sourced with care and delivered to your doorstep to make everyday shopping simple, fast, and healthy.
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { GoHeartFill } from "react-icons/go";
 import { RiShoppingBasketFill } from "react-icons/ri";
 import { IoSearch } from "react-icons/io5";
+import { IoMenu } from "react-icons/io5";
+import { CgMenuMotion } from "react-icons/cg";
 
 const Navbar = () => {
+
+  const [Showmenu, SetShowmenu] = useState(false);
+
+  const toggleMenu = () =>{
+
+    SetShowmenu(!Showmenu);
+  }
+
   return (
     <div className='bg-white fixed top-0 left-0 right-0'>
-      <nav className='flex justify-between max-w-[1200px] h-[22vh] px-10 items-center mx-auto'>
+      <nav className='flex justify-between max-w-[1200px] md:h-[22vh] h-[10vh] px-10 items-center mx-auto'>
         {/* Logo Section */}
         <header>
           <NavLink to={"/"}>
@@ -18,7 +28,7 @@ const Navbar = () => {
         </header>
 
         {/* Navbar Tabs */}
-        <div className='flex items-center gap-16 font-semibold tracking-wider'>
+        <div className='md:flex items-center gap-16 font-semibold tracking-wider hidden'>
           <ul>
             <li>
               <NavLink
@@ -52,7 +62,7 @@ const Navbar = () => {
 
         {/* Search Bar and Favourites */}
         <div className='flex items-center gap-x-3'>
-          <div className='flex p-2 border-2 rounded-3xl items-center border-orange-500'>
+          <div className='md:flex p-2 border-2 rounded-3xl items-center border-orange-500 hidden'>
             <input type="text" name='text' id='text' placeholder='Search...' autoComplete='off' className='flex h-[3vh] px-3 focus:outline-none' />
             <button className='bg-gradient-to-b from-orange-600 to-orange-400 text-white h-8 w-8 flex justify-center items-center rounded-full text-sm'>
               <IoSearch />
@@ -64,7 +74,45 @@ const Navbar = () => {
           <NavLink to={'/'} className={'text-2xl text-zinc-900'}>
             <RiShoppingBasketFill />
           </NavLink>
+          <NavLink onClick={toggleMenu} className={'text-zinc-800 text-3xl md:hidden'}>
+            {Showmenu?<CgMenuMotion /> : <IoMenu />}
+          </NavLink>
         </div>
+
+        {/* Mobile Nav */}
+
+          <ul className={`flex flex-col absolute items-center gap-16 bg-orange-300/30 backdrop-blur-xl rounded-2xl p-10 font-semibold tracking-wider md:hidden top-30 -left-full transition-all duration-500 ${Showmenu ? "left-1/2" : ""} transform -translate-x-1/2`}>
+            <li>
+              <NavLink
+                to="/" className={({ isActive }) =>isActive ? "text-orange-500" : "text-black hover:text-orange-500 duration-300"}>
+                Home
+              </NavLink>
+            </li>
+          
+            <li>
+              <NavLink to="/about" className={({ isActive }) =>isActive ? "text-orange-500" : "text-black hover:text-orange-500 duration-300"}>
+                About
+              </NavLink>
+            </li>
+          
+            <li>
+              <NavLink to="/process" className={({ isActive }) =>isActive ? "text-orange-500" : "text-black hover:text-orange-500 duration-300"}>
+                Process
+              </NavLink>
+            </li>
+          
+            <li>
+              <NavLink to="/contact" className={({ isActive }) =>isActive ? "text-orange-500" : "text-black hover:text-orange-500 duration-300"}>
+                Contact-us
+              </NavLink>
+            </li>
+            <li className='flex p-2 border-2 rounded-3xl items-center border-black-700 md:hidden'>
+            <input type="text" name='text' id='text' placeholder='Search...' autoComplete='off' className='flex h-[3vh] px-3 focus:outline-none' />
+            <button className='bg-gradient-to-b from-orange-600 to-orange-400 text-white h-8 w-8 flex justify-center items-center rounded-full text-sm'>
+              <IoSearch />
+            </button>
+          </li>
+          </ul>
       </nav>
     </div>
   )
