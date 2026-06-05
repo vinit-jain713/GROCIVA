f9385fb92d4d93f2b8001365f2b282e63665a26d
vinit-jain713 <jainvinit713@gmail.com>
2026-06-05 21:07:38 +0530

    Optimize images

--- src/Components/Values/Values.jsx
+++ src/Components/Values/Values.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Heading from '../Heading/Heading'
 import { FaHeart, FaLeaf, FaSeedling, FaShieldAlt } from 'react-icons/fa'
-import Basket from '../../assets/basket-full-vegetables.png'
+import Basket from '../../assets/basket-full-vegetables.webp'
 
 const Values = () => {
 
